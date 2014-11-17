3412cb63f44b9eda450f3639e016c28cc3c050dc
Alex Robinson <alexandra.scott335@gmail.com>
2014-11-16 17:52:50 -0700

    added intial messages to display module that will be printed to outstream in cli and tests for each method in the display module

---
+++ lib/display.rb
@@ -0,0 +1,84 @@
+module Display
+
+  def self.welcome
+    "Welcome to Event Reporter!"
+  end
+
+  def self.root_menu
+    "Press 'h' for help. Press 'q' for quit."
+  end
+
+  def self.command_prompt
+    "Enter a command: "
+  end
+
+  def self.quitting
+    "You are now exiting Event Reporter. Goodbye!"
+  end
+
+  def self.load(filename)
+    "#{filename} loaded successfully"
+  end
+
+  def self.count(number)
+    "There are #{number} results in queue"
+  end
+
+  def self.clear
+    "The queue has been cleared"
+  end
+
+  def self.placed_in_queue
+    "Your search has been placed in queue"
+  end
+
+  def self.help_menu
+      puts "Help Menu".blue.center(32,".")
+      puts "Get help on the following topics:".red
+      puts "help load/load<filename>"
+      puts "help queue count"
+      puts "help queue clear"
+      puts "help queue print"
+      puts "help queue print by <attribute>"
+      puts "help queue save to <filename.csv>"
+      puts "help find <attribute><criteria>"
+  end
+
+  def self.help_load
+    "Enter 'load' followed by the name of a file, to load information in the given"/
+    "file to search from. If no file name is specified, information from"/
+    "'event_reporter.csv' will be loaded by default."
+  end
+
+  def self.help_queue_count
+    "Enter 'queue count' to return the number of entries in the current queue."
+  end
+
+  def self.help_queue_clear
+    "Enter 'queue clear' to delete all entries in current queue."
+  end
+
+  def self.help_queue_print
+    "Enter 'queue print' to print all entries in current queue."
+  end
+
+  def self.help_queue_print_by_attribute
+    "Enter 'queue print by' followed by an attribute to print items in a queue"/
+    "sorted by the given attribute."
+  end
+
+  def self.help_queue_save_to
+    "Enter 'queue_save_to' followed by the file name and extension to save items"/
+    "in queue to the specified file."
+  end
+
+  def self.help_queue_find_by_attribute_criteria
+    "Enter 'find' followed by an attribute and criteria to return a list of items"/
+    "containing the criteria when sorted by the specified attribute."
+  end
+
+  def self.help_quit
+    "press 'q' at anytime to exit help menu"
+  end
+
+end
---
+++ test/display_test.rb
@@ -0,0 +1,76 @@
+gem 'minitest', '~> 5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/display'
+
+class DisplayTest < Minitest::Test
+
+
+  def test_Welcome_Message
+    assert_equal "Welcome to Event Reporter!", Display.welcome
+  end
+
+  def test_root_menu
+    assert_equal "Press 'h' for help. Press 'q' for quit.", Display.root_menu
+  end
+
+  def test_command_prompt
+    assert_equal "Enter a command: ", Display.command_prompt
+  end
+
+  def test_quitting
+    assert_equal "You are now exiting Event Reporter. Goodbye!", Display.quitting
+  end
+
+  def test_load
+    assert_equal "NewFile loaded successfully", Display.load("NewFile")
+  end
+
+  def test_count
+    assert_equal "There are 3 results in queue", Display.count(3)
+  end
+
+  def test_clear
+    assert_equal "The queue has been cleared", Display.clear
+  end
+
+  def test_placed_in_queue
+    assert_equal "Your search has been placed in queue", Display.placed_in_queue
+  end
+
+  def test_help_menu
+    assert Display.respond_to?(:help_menu)
+  end
+
+  def test_help_load
+    assert Display.respond_to?(:help_load)
+  end
+
+  def test_help_queue_count
+    assert Display.respond_to?(:help_queue_count)
+  end
+
+  def test_help_queue_clear
+    assert Display.respond_to?(:help_queue_clear)
+  end
+
+  def test_help_queue_print
+    assert Display.respond_to?(:help_queue_print)
+  end
+
+  def test_help_queue_print_by_attribute
+    assert Display.respond_to?(:help_queue_print_by_attribute)
+  end
+
+  def test_help_queue_save_to
+    assert Display.respond_to?(:help_queue_save_to)
+  end
+
+  def test_help_queue_find_by_attribute_criteria
+    assert Display.respond_to?(:help_queue_find_by_attribute_criteria)
+  end
+
+  def test_help_quit
+    assert Display.respond_to?(:help_quit)
+  end
+end

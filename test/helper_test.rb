87d71b477dfa40ea8c8859a5c45b6afd0ccd5888
Manny Navarro <manny.navarry@gmail.com>
2014-11-17 07:40:40 -0700

    added test_helper file - minimize need for boiler plater entry at top of test files.

---
+++ test/helper_test.rb
@@ -0,0 +1,42 @@
+gem 'minitest', '~> 5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/helper'
+
+class HelperTest < Minitest::Test
+
+  def test_quit?
+    help = Helper.new("q")
+    assert help.quit?
+  end
+
+  def test_help_menu?
+    help = Helper.new('help')
+    assert help.help_menu?
+  end
+
+  def test_load?
+    help = Helper.new('load')
+    assert help.help_load?
+  end
+
+  def test_help_queue_count?
+    help = Helper.new('queue count')
+    assert help.help_queue_count?
+  end
+
+  def test_help_queue_clear?
+    help = Helper.new('help queue clear')
+    assert help.help_queue_clear?
+  end
+
+  def test_help_queue_print?
+    help = Helper.new('help queue print')
+    assert help.help_queue_print?
+  end
+
+  def test_help_queue_save_to?
+    help = Helper.new('queue save to')
+    assert help.help_queue_save_to?
+  end
+end
